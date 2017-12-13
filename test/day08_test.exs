177f005a0dbfee5b506a79818c28d4c97659d5c5
Steven Martin <me@stevenmartin.io>
2017-12-13 08:25:44 -0500

    Skipping longer running tests

--- test/day08_test.exs
+++ test/day08_test.exs
@@ -4,14 +4,14 @@ defmodule AdventOfCode2018.Day08Test do
 
     import Day08
 
-    # @tag :skip
+    @tag :skip
     test "Part 1 works" do
         assert with_puzzle_input("test/input/day08.txt", fn input ->
             assert 5221 == input |> part1
         end)
     end
 
-    # @tag :skip
+    @tag :skip
     test "Part 2 works" do
         assert with_puzzle_input("test/input/day08.txt", fn input ->
             assert 7491 == input |> part2
