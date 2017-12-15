8f52048c230140f06a2e901b07de00b3587f57d6
Steven Martin <me@stevenmartin.io>
2017-12-15 09:37:48 -0500

    Day 11 part 2

--- lib/day11.ex
+++ lib/day11.ex
@@ -1,25 +1,37 @@
 defmodule Day11 do
     def part1(input) do
         input
+        |> parse_input
+        |> Enum.reduce({0, 0, 0}, &move/2)
+        |> cube_distance({0, 0, 0})
+    end
+
+    def parse_input(input) do
+        input
         |> String.trim
         |> String.split(",")
         |> Enum.map(&String.to_atom/1)
-        |> Enum.reduce({0, 0, 0}, fn 
-            (:n,  {x, y, z}) -> {x    , y + 1, z - 1}
-            (:ne, {x, y, z}) -> {x + 1, y    , z - 1}
-            (:se, {x, y, z}) -> {x + 1, y - 1, z    }
-            (:s,  {x, y, z}) -> {x    , y - 1, z + 1}
-            (:sw, {x, y, z}) -> {x - 1, y    , z + 1}
-            (:nw, {x, y, z}) -> {x - 1, y + 1, z    }
-        end)
-        |> cube_distance({0, 0, 0})
     end
+
+    def move(:n,  {x, y, z}), do: {x    , y + 1, z - 1}
+    def move(:ne, {x, y, z}), do: {x + 1, y    , z - 1}
+    def move(:se, {x, y, z}), do: {x + 1, y - 1, z    }
+    def move(:s,  {x, y, z}), do: {x    , y - 1, z + 1}
+    def move(:sw, {x, y, z}), do: {x - 1, y    , z + 1}
+    def move(:nw, {x, y, z}), do: {x - 1, y + 1, z    }
 
     def cube_distance({ax, ay, az}, {bx, by, bz}) do
         div(abs(ax - bx) + abs(ay - by) + abs(az - bz), 2)
     end
 
-    def part2(_input) do
-
+    def part2(input) do
+        input
+        |> parse_input
+        |> Enum.reduce({{0, 0, 0}, 0}, fn(direction, {location, farthest}) ->
+            new_loc = move(direction, location)
+            distance_from_home = cube_distance(new_loc, {0, 0, 0})
+            {new_loc, if(distance_from_home > farthest, do: distance_from_home, else: farthest)}
+        end)
+        |> elem(1)
     end
 end
--- test/day11_test.exs
+++ test/day11_test.exs
@@ -4,7 +4,7 @@ defmodule AdventOfCode2017.Day11Test do
 
     import Day11
 
-    # @tag :skip
+    @tag :skip
     test "Part 1 works" do
         assert with_puzzle_input("test/input/day11.txt", fn input ->
             assert 675 == input |> part1
@@ -14,7 +14,7 @@ defmodule AdventOfCode2017.Day11Test do
     @tag :skip
     test "Part 2 works" do
         assert with_puzzle_input("test/input/day11.txt", fn input ->
-            assert 0 == input |> part2
+            assert 1424 == input |> part2
         end)
     end
 
