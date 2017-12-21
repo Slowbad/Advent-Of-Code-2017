08d998a536097ddd6f2618274d7a9bb030b142ec
Steven Martin <me@stevenmartin.io>
2017-12-21 09:32:34 -0500

    Day 17 part 2

--- lib/day17.ex
+++ lib/day17.ex
@@ -1,14 +1,27 @@
 defmodule Day17 do
   def part1(spin_size) do
-    {buffer, position} = Enum.reduce(1..2017, {[0], 0}, fn(n, {buffer, position}) ->
-      insert(buffer, position, n, spin_size)
-    end)
+    {buffer, position} = spinlock(2017, spin_size)
 
     Enum.at(buffer, position + 1)
   end
 
-  def part2(_input) do
+  # Only keep track of the value next to zero. The rest of buffer doesn't matter.
+  def part2(spin_size) do
+    Enum.reduce(1..50_000_000, {0, nil}, fn(val, {position, beside_zero}) ->
+      next_position = rem(position + spin_size, val) + 1
+      beside_zero = if next_position == 1, do: val, else: beside_zero
+      {next_position, beside_zero}
+    end)
+    |> elem(1)
   end
+
+  # naive implementation
+  def spinlock(iterations, spin_size) do
+    Enum.reduce(1..iterations, {[0], 0}, fn(n, {buffer, position}) ->
+      insert(buffer, position, n, spin_size)
+    end)
+  end
+
 
   @doc """
       iex> Day17.insert([0], 0, 1, 3)
@@ -22,7 +35,6 @@ defmodule Day17 do
   """
   def insert(buffer, position, next_value, spin_size) do
     new_position = rem(position + spin_size, length(buffer)) + 1
-    {front, back} = Enum.split(buffer, new_position)
-    {front ++ [next_value] ++ back, new_position}
+    {List.insert_at(buffer, new_position, next_value), new_position}
   end
 end
--- test/day17_test.exs
+++ test/day17_test.exs
@@ -6,13 +6,13 @@ defmodule AdventOfCode2017.Day17Test do
 
     @input 366
 
-    # @tag :skip
+    @tag :skip
     test "Part 1 works" do
       assert 1025 == @input |> part1
     end
 
     @tag :skip
     test "Part 2 works" do
-      assert -1 == @input |> part2
+      assert 37803463 == @input |> part2
     end
 end
