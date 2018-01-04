d705f9282a9813dd5bb80d99ce617c9a2369366b
Steven Martin <me@stevenmartin.io>
2018-01-03 20:37:44 -0500

    Day 22 part 2

--- lib/day22.ex
+++ lib/day22.ex
@@ -1,18 +1,26 @@
 defmodule Day22 do
-  def part1(input, iterations \\ 10000) do
+  def part1(input, iterations \\ 10_000) do
     grid = input |> parse_input
 
     Stream.iterate({grid, {0, 0}, :up, 0}, fn
       ({g, n, d, c}) ->
-        {g, n, d, infected?} = burst(g, n, d)
-        #{g, n |> IO.inspect(label: "node"), d |> IO.inspect(label: "direction"), c + if(infected? |> IO.inspect(label: "infected?"), do: 1, else: 0)}
+        {g, n, d, infected?} = burst(g, n, d, false)
         {g, n, d, c + if(infected?, do: 1, else: 0)}
     end)
     |> Enum.at(iterations)
     |> elem(3)
   end
 
-  def part2(_) do
+  def part2(input, iterations \\ 10_000_000) do
+    grid = input |> parse_input
+
+    Stream.iterate({grid, {0, 0}, :up, 0}, fn
+      ({g, n, d, c}) ->
+        {g, n, d, infected?} = burst(g, n, d, true)
+        {g, n, d, c + if(infected?, do: 1, else: 0)}
+    end)
+    |> Enum.at(iterations)
+    |> elem(3)
   end
 
   def parse_input(input) do
@@ -44,15 +52,15 @@ defmodule Day22 do
   end
 
   @doc """
-      iex> Day22.burst(%{{0, 0} => :infected}, {0, 0}, :up)
+      iex> Day22.burst(%{{0, 0} => :infected}, {0, 0}, :up, false)
       {%{{0, 0} => :clean}, {1, 0}, :right, false}
 
-      iex> Day22.burst(%{{0, 0} => :clean}, {0, 0}, :up)
+      iex> Day22.burst(%{{0, 0} => :clean}, {0, 0}, :up, false)
       {%{{0, 0} => :infected}, {-1, 0}, :left, true}
   """
-  def burst(grid, current_node, direction) do
+  def burst(grid, current_node, direction, advanced?) do
     new_direction = turn(grid, current_node, direction)
-    {updated_grid, node_infected} = scramble(grid, current_node)
+    {updated_grid, node_infected} = scramble(grid, current_node, advanced?)
     next_node = move(current_node, new_direction)
     {updated_grid, next_node, new_direction, node_infected}
   end
@@ -61,6 +69,8 @@ defmodule Day22 do
     case Map.get(grid, node, :clean) do
       :infected -> right(direction)
       :clean -> left(direction)
+      :weakened -> direction
+      :flagged -> reverse(direction)
     end
   end
 
@@ -74,12 +84,30 @@ defmodule Day22 do
   def left(:down), do: :right
   def left(:left), do: :down
 
-  def scramble(grid, node) do
-    flipped_state = case Map.get(grid, node, :clean) do
+  def reverse(:up), do: :down
+  def reverse(:down), do: :up
+  def reverse(:right), do: :left
+  def reverse(:left), do: :right
+
+  def scramble(grid, node, advanced?) do
+    flipped_state = next_state(grid, node, advanced?)
+    {Map.put(grid, node, flipped_state), flipped_state == :infected}
+  end
+
+  def next_state(grid, node, advanced?) when not advanced? do
+    case Map.get(grid, node, :clean) do
       :clean -> :infected
       :infected -> :clean
     end
-    {Map.put(grid, node, flipped_state), flipped_state == :infected}
+  end
+
+  def next_state(grid, node, advanced?) when advanced? do
+    case Map.get(grid, node, :clean) do
+      :clean -> :weakened
+      :weakened -> :infected
+      :infected -> :flagged
+      :flagged -> :clean
+    end
   end
 
   def move({x, y}, direction) do
--- test/day22_test.exs
+++ test/day22_test.exs
@@ -4,7 +4,7 @@ defmodule AdventOfCode2017.Day22Test do
 
     import Day22
 
-    #@tag :skip
+    @tag :skip
     test "Part 1 works" do
       assert with_puzzle_input("test/input/day22.txt", fn input ->
         assert 5261 == input |> part1
@@ -14,7 +14,7 @@ defmodule AdventOfCode2017.Day22Test do
     @tag :skip
     test "Part 2 works" do
       assert with_puzzle_input("test/input/day22.txt", fn input ->
-        assert -1 == input |> part2
+        assert 2511927 == input |> part2
       end)
     end
 end
